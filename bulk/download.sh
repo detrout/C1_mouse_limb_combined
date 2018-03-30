12fc7c6c71f1c4ba345d421102d3b19040f28c7b
Diane Trout <diane@ghic.org>
2018-03-30 15:26:25 -0700

    Document making the bulk combined coverage file.

---
+++ bulk/download.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mkdir 15019
+curl -L -o 15019/15019-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF748RBN/@@download/ENCFF748RBN.bam
+mkdir 15020
+curl -L -o 15020/15020-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF271VQQ/@@download/ENCFF271VQQ.bam
+mkdir 15084
+curl -L -o 15084/15084-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF483ZFR/@@download/ENCFF483ZFR.bam
+mkdir 15085
+curl -L -o 15085/15085-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF793SNG/@@download/ENCFF793SNG.bam
+mkdir 16111
+curl -L -o 16111/16111-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF943SLL/@@download/ENCFF943SLL.bam
+mkdir 16112
+curl -L -o 16112/16112-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF808VGK/@@download/ENCFF808VGK.bam
+mkdir 16134
+curl -L -o 16134/16134-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF445SGU/@@download/ENCFF445SGU.bam
+mkdir 16135
+curl -L -o 16135/16135-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF223JGE/@@download/ENCFF223JGE.bam
+mkdir 16930
+curl -L -o 16930/16930-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF766OUK/@@download/ENCFF766OUK.bam
+mkdir 16931
+curl -L -o 16931/16931-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF188HWM/@@download/ENCFF188HWM.bam
+mkdir 17298
+curl -L -o 17298/17298-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF309GAT/@@download/ENCFF309GAT.bam
+mkdir 17299
+curl -L -o 17299/17299-mm10-M4-male_anno.bam https://www.encodeproject.org/files/ENCFF836HAC/@@download/ENCFF836HAC.bam
