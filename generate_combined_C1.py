7ef7193ff1f009d720a0d0d9054b95c4c02e0a4f
Diane Trout <diane@ghic.org>
2018-04-24 11:23:41 -0700

    Sean only kept the combined transcription files with the gene names
    So I needed to switch what files I was loading.

--- generate_combined_C1.py
+++ generate_combined_C1.py
@@ -16,19 +16,19 @@ paper_433_FPKM_files = """~sau/flowcells/C1_e10.5_mouse_limb_run2_June20_2016/C1
 
 ASOF_RUN17_FPKM_FILES = paper_433_FPKM_files + """
     ~sau/flowcells/HFNLNBCX2/C1_mouse_e12.5_forelimb_run9_March8_2017_rearray_March152018_gene_FPKM.csv
-    ~sau/flowcells/H7CNTBCX2/C1_mouse_e11.5_forelimb_run10_December11_2017_gene_FPKM.csv
-    ~sau/flowcells/HFNLTBCX2/C1_mouse_e12.0_forelimb_run11_December12_2017_gene_FPKM.csv
-    ~sau/flowcells/HF7NTBCX2/C1_mouse_e12.0_forelimb_run11_December12_2017_gene_FPKM.csv
-    ~sau/flowcells/HF7NTBCX2/C1_mouse_e13.0_forelimb_run12_December13_2017_gene_FPKM.csv
-    ~sau/flowcells/HFNLNBCX2/C1_mouse_e14.0_forelimb_run13_December14_2017_gene_FPKM.csv
-    ~sau/flowcells/HFNYNBCX2/C1_mouse_e15.5_forelimb_run14_December15_2017_gene_FPKM.csv
-    ~sau/flowcells/H7CNTBCX2/C1_mouse_e10.5_forelimb_run15_January13AM_2018_gene_FPKM.csv
-    ~sau/flowcells/H7CNTBCX2/C1_mouse_e11.0_forelimb_run16_January13PM_2018_gene_FPKM.csv
-    ~sau/flowcells/H7CNTBCX2/C1_mouse_e14.5_forelimb_run17_January16_2018_gene_FPKM.csv
+    ~sau/flowcells/H7CNTBCX2/C1_mouse_e11.5_forelimb_run10_December11_2017_gene_FPKM_names.csv
+    ~sau/flowcells/HFNLTBCX2/C1_mouse_e12.0_forelimb_run11_December12_2017_gene_FPKM_names.csv
+    ~sau/flowcells/HF7NTBCX2/C1_mouse_e12.0_forelimb_run11_December12_2017_gene_FPKM_names.csv
+    ~sau/flowcells/HF7NTBCX2/C1_mouse_e13.0_forelimb_run12_December13_2017_gene_FPKM_names.csv
+    ~sau/flowcells/HFNLNBCX2/C1_mouse_e14.0_forelimb_run13_December14_2017_gene_FPKM_names.csv
+    ~sau/flowcells/HFNYNBCX2/C1_mouse_e15.5_forelimb_run14_December15_2017_gene_FPKM_names.csv
+    ~sau/flowcells/H7CNTBCX2/C1_mouse_e10.5_forelimb_run15_January13AM_2018_gene_FPKM_names.csv
+    ~sau/flowcells/H7CNTBCX2/C1_mouse_e11.0_forelimb_run16_January13PM_2018_gene_FPKM_names.csv
+    ~sau/flowcells/H7CNTBCX2/C1_mouse_e14.5_forelimb_run17_January16_2018_gene_FPKM_names.csv
 """
 
 def main():
-    make_paper_combined()
+    #make_paper_combined()
     make_asof_run17_combined()
 
 
@@ -50,17 +50,27 @@ def make_asof_run17_combined():
 
 def make_combined(quant_files, to_include):
     sheets = None
+    gene_name = None
     for f in quant_files.split():
         f = os.path.expanduser(f.strip())
         if sheets is None:
             sheets = pandas.read_csv(f)
         else:
-            sheets = pandas.merge(sheets, pandas.read_csv(f), on='gene_id')
+            sheet = pandas.read_csv(f)
+            columns = [ x.replace('_mm10', '').replace('_clean', '') for x in sheet.columns]
+            sheet.columns = columns
+            if 'gene_name' in sheet.columns:
+                gene_name = sheet.gene_name
+                sheet = sheet.drop('gene_name', axis=1)
+            sheets = pandas.merge(sheets, sheet, on='gene_id')
 
     print(sheets.columns)
     print(sheets.shape)
+    print('to_include', to_include)
+    print('columns', list(sheets.columns))
     print(sheets[to_include].shape)
     filtered = sheets[to_include]
+    filtered = sheets
     newsheet = filtered.set_index('gene_id')
     print(newsheet.shape)
     return newsheet
