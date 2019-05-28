31dbb6743db7f40047beab9ce216e2b8942a15b5
Diane Trout <diane@ghic.org>
2019-05-28 14:46:27 -0700

    Add docs and change median line to a different grey
    when given a black cluster

--- c1extra.py
+++ c1extra.py
@@ -1,18 +1,16 @@
-"""Custom seaborn plot for C1 box plot figures
+"""
 
-They want one of the clusters to be black, so we need to
-turn the median line white.
 """
 import seaborn
 from matplotlib import pyplot as plt
 
 
 class C1BoxPlotter(seaborn.categorical._BoxPlotter):
+    """Custom seaborn plot for C1 box plot figures"""
     def restyle_boxplot(self, artist_dict, color, props):
         super(C1BoxPlotter, self).restyle_boxplot(artist_dict, color, props)
-        color = '#C0C0C0'  #'#717E8D'
+        color = '#C0C0C0'
         if artist_dict['boxes'][0].get_facecolor() == (0.0, 0.0, 0.0, 1.0):
-            #artist_dict['boxes'][0].set_edgecolor(color)
             artist_dict['medians'][0].set_color(color)
 
 
@@ -20,6 +18,11 @@ def boxplot(x=None, y=None, hue=None, data=None, order=None, hue_order=None,
             orient=None, color=None, palette=None, saturation=.75,
             width=.8, dodge=True, fliersize=5, linewidth=None,
             whis=1.5, notch=False, ax=None, **kwargs):
+    """Custom seaborn plot for C1 box plot figures
+
+    They want one of the clusters to be black, so we need to
+    turn the median line white.
+    """
 
     plotter = C1BoxPlotter(x, y, hue, data, order, hue_order,
                            orient, color, palette, saturation,
